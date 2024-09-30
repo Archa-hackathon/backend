aaa7465ff81f0737fe6c2b8f10f2b7e2200d1605
robinafro <oble.ro.2023@skola.ssps.cz>
2024-09-30 17:58:47 +0200

    create_order endpoint WIP

--- src/apps/piticko/main.py
+++ src/apps/piticko/main.py
@@ -3,12 +3,21 @@ from flask import Blueprint, jsonify, request
 piticko = Blueprint("bar", __name__)
 
 ORDERS = []  # Will be a database later?
+ALLOWED_ITEMS["pizza", "beer", "burger"]
 
 
 @piticko.route("/create_order", methods=["POST"])
 def create_order():
-    data = request.get_json()
+    order = request.json
 
+    if not "items" in order:
+        return jsonify({"error": "items is required"}), 400
+
+    for item in order["items"]:
+        if item not in ALLOWED_ITEMS:
+            return jsonify({"error": f"{item} is not allowed"}), 400
+
+    order_obj = {"id": 1}  # Calculate id later
 
 # ADMIN
 
