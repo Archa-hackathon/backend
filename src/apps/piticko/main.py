2f4934852eeea6d4367c43fe5148f8f654955f1a
robinafro <oble.ro.2023@skola.ssps.cz>
2024-09-30 16:13:18 +0200

    create_order endpoint finished

--- src/apps/piticko/main.py
+++ src/apps/piticko/main.py
@@ -1,3 +1,5 @@
+import uuid
+
 from flask import Blueprint, jsonify, request
 
 piticko = Blueprint("bar", __name__)
@@ -6,18 +8,39 @@ ORDERS = []  # Will be a database later?
 ALLOWED_ITEMS = ["pizza", "beer", "burger"]
 
 
+class Order:
+    def __init__(self, items):
+        self.id = str(uuid.uuid4())
+        self.secret_id = str(uuid.uuid4())
+        self.items = items
+        self.finished = False
+
+    def dict(self):
+        return {
+            "id": self.id,
+            "secret_id": self.secret_id,
+            "finished": self.finished,
+            "items": self.items,
+        }
+
+
 @piticko.route("/create_order", methods=["POST"])
 def create_order():
     order = request.json
 
-    if not "items" in order:
+    if "items" not in order:
         return jsonify({"error": "items is required"}), 400
 
     for item in order["items"]:
         if item not in ALLOWED_ITEMS:
             return jsonify({"error": f"{item} is not allowed"}), 400
 
-    order_obj = {"id": 1}  # Calculate id later
+    order = Order(order["items"])
+
+    ORDERS.append(order)
+
+    # Return only the secret id
+    return jsonify({"secret_id": order.secret_id, "success": True})
 
 
 # ADMIN
