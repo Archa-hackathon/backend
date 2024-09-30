3f303e487a34fca3722b3ce0113ee780010722e5
robinafro <oble.ro.2023@skola.ssps.cz>
2024-09-30 17:19:34 +0200

    added db operation methods

--- src/apps/filter/main.py
+++ src/apps/filter/main.py
@@ -1,7 +1,25 @@
-from flask import Blueprint, jsonify, request
+import sqlite3
 
+from flask import Blueprint, current_app, g, jsonify, request
 
 filtr = Blueprint("filter", __name__)
+
+
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect(
+            current_app.config["DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+
+def close_db(e=None):
+    db = g.pop("db", None)
+
+    if db is not None:
+        db.close()
 
 
 @filtr.route("/question", methods=["POST"])
