36c4d802845a36f793e6594e49db99418fb5ebe9
robinafro <oble.ro.2023@skola.ssps.cz>
2024-09-30 14:44:18 +0200

    Starter flask code

--- src/main.py
+++ src/main.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
 
-print("Hello there!")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
